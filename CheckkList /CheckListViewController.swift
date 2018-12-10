918e4db1f4816da76e18c7c13b3fa558e2635640
Andrii <admiralurun@gmail.com>
2018-12-10 15:24:51 +0200

    Star nw project checklist App

---
+++ CheckkList /CheckListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  CheckkList
+//
+//  Created by Andrew Yakovenko on 12/10/18.
+//  Copyright © 2018 Andrew Yakovenko. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        
+        if let lable = cell.viewWithTag(1000) as? UILabel {
+            if indexPath.row == 0 {
+                lable.text = "Run at morning"
+            } else {
+                lable.text = "Sleep"
+            }
+        }
+        return cell
+    }
+    
+    
+
+}
+
